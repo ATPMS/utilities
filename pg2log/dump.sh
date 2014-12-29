7e9bffb26b3f50a80c18aa5d07a75e29a1b13d2f
Wilhansen Li <wil@byimplication.com>
2014-12-29 21:25:54 +0800

    Fix stupid dump.sh typo.

--- pg2log/dump.sh
+++ pg2log/dump.sh
@@ -6,5 +6,5 @@ psql $@ <<'HERE' | ./pg2log -dest="${DEST}"
 COPY (
 		SELECT id, vessel_id, session_token, EXTRACT(EPOCH FROM logged_at) AS timestamp, lon, lat
 			FROM logs ORDER BY logged_at ASC
-	) TO STDOUT WITH DELIMITER ' '"
+	) TO STDOUT WITH DELIMITER ' '
 HERE
