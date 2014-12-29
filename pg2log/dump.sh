4d7b54117ce199d0b48c21deeea22c723ac84286
Wilhansen Li <wil@byimplication.com>
2014-12-29 21:23:24 +0800

    Fix dump script.

--- pg2log/dump.sh
+++ pg2log/dump.sh
@@ -2,7 +2,7 @@
 
 DEST=raw_logs.txt
 
-psql $@ -c <<'HERE' | ./pg2log -dest="${DEST}"
+psql $@ <<'HERE' | ./pg2log -dest="${DEST}"
 COPY (
 		SELECT id, vessel_id, session_token, EXTRACT(EPOCH FROM logged_at) AS timestamp, lon, lat
 			FROM logs ORDER BY logged_at ASC
